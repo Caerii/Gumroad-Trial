c06b3dbd095dc8106a5d59dec177ace180e37815
Caerii <cubelocked@gmail.com>
2024-01-13 18:00:23 -0500

    some of the maincontent stuff

--- gumroad/src/components/MainContent.js
+++ gumroad/src/components/MainContent.js
@@ -1,16 +1,20 @@
-// MainContent.js
 import React from 'react';
 import './MainContent.css';
 import WelcomeBanner from './WelcomeBanner';
+// import other necessary components
 
 const MainContent = () => {
   return (
     <div className="main-content">
-      {/* Add your main dashboard content here */}
-      <WelcomeBanner  />
-      
-      <h1>Welcome to Gumroooooaaaad.</h1>
+      <WelcomeBanner />
+      <h1>Welcome to Gumroad.</h1>
+      <section className="dashboard-intro">
+        
+        <p>We're here to help you get paid for your work.</p>
+        <button className="create-product-btn">Create your first product</button>
+      </section>
       {/* Add other components like analytics insights, product lists, etc */}
+      {/* ... */}
     </div>
   );
 };
--- gumroad/src/components/Sidebar.js
+++ gumroad/src/components/Sidebar.js
@@ -1,31 +1,57 @@
 // Sidebar.js
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHome, faBoxOpen, faCashRegister, faEnvelope, faCog,
+  faChartLine, faDollarSign, faSearch, faBook, faPlusSquare,
+  faQuestionCircle, faUser
+} from '@fortawesome/free-solid-svg-icons';
 import './Sidebar.css';
 
 const Sidebar = () => {
   return (
     <aside className="sidebar">
-        <a href="/" className="logo-link">
-                <img src={`${process.env.PUBLIC_URL}/invert.png`} alt="Gumroad Logo" className="sidebar-logo"/>
-        </a>
-      <a href="#home" className="sidebar-item">Home</a>
-      <a href="#products" className="sidebar-item">Products</a>
-      <a href="#checkout" className="sidebar-item">Checkout</a>
-      <a href="#emails" className="sidebar-item">Emails</a>
-      {/* workflows, sales, analytics, blank, payouts, discover, library, blank, start selling, help, settings, user profile */}
-      <a href="#workflows" className="sidebar-item">Workflows</a>
-        <a href="#sales" className="sidebar-item">Sales</a>
-        <a href="#analytics" className="sidebar-item">Analytics</a>
-        <a href="#home" className="sidebar-item">Blank</a>
-        <a href="#payouts" className="sidebar-item">Payouts</a>
-        <a href="#discover" className="sidebar-item">Discover</a>
-        <a href="#library" className="sidebar-item">Library</a>
-        <a href="#home" className="sidebar-item">Blank</a>
-        <a href="#start-selling" className="sidebar-item">Start Selling</a>
-        <a href="#help" className="sidebar-item">Help</a>
-        <a href="#settings" className="sidebar-item">Settings</a>
-        <a href="#user-profile" className="sidebar-item">User Profile</a>
-      {/* Repeat for other items as per your mockup */}
+      <div className="sidebar-logo-container">
+        <img src={`${process.env.PUBLIC_URL}/invert.png`} alt="Gumroad Logo" className="sidebar-logo"/>
+      </div>
+      <div className="sidebar-menu">
+        <div className="sidebar-separator"></div>
+
+        <a href="#home" className="sidebar-item"><FontAwesomeIcon icon={faHome} /> Home</a>
+        <div className="sidebar-separator"></div>
+        <a href="#products" className="sidebar-item"><FontAwesomeIcon icon={faBoxOpen} /> Products</a>
+        <div className="sidebar-separator"></div>
+        <a href="#checkout" className="sidebar-item"><FontAwesomeIcon icon={faCashRegister} /> Checkout</a>
+        <div className="sidebar-separator"></div>
+        <a href="#emails" className="sidebar-item"><FontAwesomeIcon icon={faEnvelope} /> Emails</a>
+        <div className="sidebar-separator"></div>
+        <a href="#workflows" className="sidebar-item"><FontAwesomeIcon icon={faCog} /> Workflows</a>
+        <div className="sidebar-separator"></div>
+        <a href="#sales" className="sidebar-item"><FontAwesomeIcon icon={faChartLine} /> Sales</a>
+        <div className="sidebar-separator"></div>
+        <a href="#analytics" className="sidebar-item"><FontAwesomeIcon icon={faDollarSign} /> Analytics</a>
+        {/* Another blank div as a separator */}
+        <div className="sidebar-separator"></div>
+        <div className="sidebar-item-blank"></div>
+        <div className="sidebar-separator"></div>
+        <a href="#payouts" className="sidebar-item"><FontAwesomeIcon icon={faSearch} /> Payouts</a>
+        <div className="sidebar-separator"></div>
+        <a href="#discover" className="sidebar-item"><FontAwesomeIcon icon={faBook} /> Discover</a>
+        <div className="sidebar-separator"></div>
+        <a href="#library" className="sidebar-item"><FontAwesomeIcon icon={faPlusSquare} /> Library</a>
+        <div className="sidebar-separator"></div>
+        <div className="sidebar-item-blank_2"></div>
+        <div className="sidebar-separator"></div>
+        <a href="#start-selling" className="sidebar-item"><FontAwesomeIcon icon={faQuestionCircle} /> Start Selling</a>
+        <div className="sidebar-separator"></div>
+        <a href="#help" className="sidebar-item"><FontAwesomeIcon icon={faUser} /> Help</a>
+        <div className="sidebar-separator"></div>
+        <a href="#settings" className="sidebar-item"><FontAwesomeIcon icon={faCog} /> Settings</a>
+        <div className="sidebar-separator"></div>
+        <a href="#user-profile" className="sidebar-item"><FontAwesomeIcon icon={faUser} /> Al J</a>
+
+        {/* Add other menu items as needed */}
+      </div>
     </aside>
   );
 };
